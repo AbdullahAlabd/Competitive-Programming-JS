ac98649712fc9819ea79b2392711575a1691be96
Abdullah Al-Abd <abdaullah.el.abd@gmail.com>
2019-05-30 10:35:32 +0200

    Create my_template.js

---
+++ my_template.js
@@ -0,0 +1,37 @@
+'use strict'
+
+function main() {
+  // main: main function, your code goes here.
+  
+}
+
+//process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+let input = "";
+process.stdin.on('data', (chunk) => input += chunk);
+
+// next: reads next value from input.
+let inputCursor = 0;
+let next = () => {
+  return inputCursor < input.length ? input[inputCursor++] : ''; 
+};
+
+//print: print one or more space seperated values.
+let print = (...data) => {
+  for(let i = 0; i < data.length; i++) {
+    process.stdout.write('' + data[i] + (i < data.length - 1 ? ' ' : ''));
+  }
+};
+
+//println: print one or more space seperated values then a line feed.
+let println = (...data) => {
+  print(...data);
+  print('\n');
+};
+
+//after the the input is read.
+process.stdin.on('end', () => {
+    input = input.trim().split(/[\s]+/);
+    main();
+});
