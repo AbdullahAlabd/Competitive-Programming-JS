839f1374b634b24f44c98fd311973521944cfb1e
Abdullah Al-Abd <abdaullah.el.abd@gmail.com>
2019-05-30 10:56:14 +0200

    Create [Div3]1165A-Remainder.js

---
+++ [Div3]1165A-Remainder.js
@@ -0,0 +1,46 @@
+'use strict'
+
+function main() {
+  let n, x, y, s;
+  n = parseInt(next()), x = parseInt(next()), y = parseInt(next()), s = next();
+  let toBeChanged = 0;
+  for(let i = n-x; i < n; i++) {
+    if(i != n-y-1) {
+      toBeChanged += s[i] == '1';
+    } else {
+      toBeChanged += s[i] == '0';
+    }
+  }
+  println(toBeChanged);
+}
+
+//process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+let input = "";
+process.stdin.on('data', (chunk) => input += chunk);
+
+// next: reads next value from input.
+let inputCursor = 0;
+let next = () => {
+  return inputCursor < input.length ? input[inputCursor++] : ''; 
+};
+
+//print: print one or more space seperated values.
+let print = (...data) => {
+  for(let i = 0; i < data.length; i++) {
+    process.stdout.write('' + data[i] + (i < data.length - 1 ? ' ' : ''));
+  }
+};
+
+//println: print one or more space seperated values then a line feed.
+let println = (...data) => {
+  print(...data);
+  print('\n');
+};
+
+//after the the input is read.
+process.stdin.on('end', () => {
+    input = input.trim().split(/[\s]+/);
+    main();
+});
