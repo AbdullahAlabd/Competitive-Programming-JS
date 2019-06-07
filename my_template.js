bc117b86a389736aa900d4efa7120a219e5db85f
Abdullah Al-Abd <abdaullah.el.abd@gmail.com>
2019-06-07 19:35:20 +0200

    Update my_template.js

--- my_template.js
+++ my_template.js
@@ -13,19 +13,20 @@ process.stdin.on('data', (chunk) => input += chunk);
 
 // next: reads next value from input.
 let inputCursor = 0;
-let next = () => {
+const next = () => {
   return inputCursor < input.length ? input[inputCursor++] : ''; 
 };
+const nextInt = () => parseInt(next());
 
 //print: print one or more space seperated values.
-let print = (...data) => {
+const print = (...data) => {
   for(let i = 0; i < data.length; i++) {
     process.stdout.write('' + data[i] + (i < data.length - 1 ? ' ' : ''));
   }
 };
 
 //println: print one or more space seperated values then a line feed.
-let println = (...data) => {
+const println = (...data) => {
   print(...data);
   print('\n');
 };
